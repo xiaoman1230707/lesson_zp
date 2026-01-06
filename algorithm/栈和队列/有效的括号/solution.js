b5e8f4f0adf6587a36f6048f5cc2d3d617f17840
liuxiaoman <liuxiaoman1230707@163.com>
2026-01-06 23:51:55 +0800

    2026-1-6 算法

---
+++ algorithm/栈和队列/有效的括号/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    for(let i = 0; i < s.length;i++){
+        let c = s[i];
+        switch(c){
+        case '(':
+            stack.push(')');
+            break;
+        case '[':
+            stack.push(']');
+            break;
+        case '{':
+            stack.push('}');
+            break;
+        default:
+            if(stack.pop() !== c)
+            return false;
+        }
+    }
+     return stack.length === 0;
+};
